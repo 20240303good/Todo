1005d37b965db851f7f89470b919a97872dd0f90
202040303good <trmf681@gmail.com>
2024-06-10 13:16:04 +0900

    “진행2”

--- src/main/kotlin/org/example/todo1/domain/todocard/model/TodoCard.kt
+++ src/main/kotlin/org/example/todo1/domain/todocard/model/TodoCard.kt
@@ -1,8 +1,23 @@
 package org.example.todo1.domain.todocard.model
 
 import jakarta.persistence.Entity
+import java.time.LocalDateTime
 
 @Entity
-class TodoCard {
-    // 클래스 내용
+class TodoCard (
+    @Column
+    var title : String,
+
+    @Column
+    var content : String,
+
+    @CreationTimestamp
+    @Column(umpdatable = false)
+    var authorName : String
+){
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    val id: Long? = null
+
+    val createdAt: LocalDateTime = LocalDateTime.now()
 }
